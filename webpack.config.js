e16c8cc2671480acb028555b04392178efb51eab
haitamkamal <169803318+haitamkamal@users.noreply.github.com.>
2025-04-10 11:44:55 +0100

    Apply Tailwind CSS

--- webpack.config.js
+++ webpack.config.js
@@ -58,7 +58,8 @@ Encore
 
     // enables Sass/SCSS support
     //.enableSassLoader()
-    
+      .enablePostCssLoader()
+
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
